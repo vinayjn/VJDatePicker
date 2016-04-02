47e8c96492da6e91eccce2a744187f719d086a19
Vinay Jain <vinay.jn7@gmail.com>
2016-04-02 19:33:43 +0530

    Added demo class

--- VJDatePicker/ViewController.swift
+++ VJDatePicker/ViewController.swift
@@ -8,18 +8,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, VJDatePickerDelegate {
+    var picker : VJDatePicker!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        picker = VJDatePicker()
+        view.addSubview(picker)
+        picker.frame = CGRectMake(0, 0, 320, 200)
+        picker.center = view.center
+        picker.datePickerDelegate = self
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func changedDatePicker(sender: UIButton) {
+        
+        let type = VJDatePickerType(rawValue: (sender.titleLabel?.text)!)
+        picker.datePickerType = type
     }
-
-
+    
+    func dateComponentsDidChange(dateComponents: VJDateComponents) {
+        print(dateComponents)
+    }
+    
 }
 
